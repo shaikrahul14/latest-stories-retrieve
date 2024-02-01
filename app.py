e5411feb3a3580a4c0203c49425ba7c717257dfa
shaikrahul14 <87261870+shaikrahul14@users.noreply.github.com>
2024-02-01 14:33:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,33 +4,30 @@ import re
 
 app = Flask(__name__)
 
-def get_latest_stories(num_stories):
+def fetch_latest_stories(num_stories):
     base_url = "https://time.com"
     response = requests.get(base_url)
     html_content = response.text
     
-    # Extract content within the specified div tag
     div_pattern = r'<div class="partial latest-stories" data-module_name="Latest Stories">(.*?)</div>'
     div_match = re.search(div_pattern, html_content, re.DOTALL)
     
     if div_match:
         div_content = div_match.group(1)
         
-        # Regular expression to find the latest stories within the extracted div content
         story_pattern = r'<li class="latest-stories__item">\s*<a href="([^"]+)">\s*<h3 class="latest-stories__item-headline">(.*?)</h3>\s*</a>'
         stories = re.findall(story_pattern, div_content)[:num_stories]
         
-        # Construct JSON object array
-        stories_json = [{"title": title.strip(), "link": link} for link, title in stories]
+        latest_stories = [{"title": title.strip(), "link": link} for link, title in stories]
         
-        return stories_json
+        return latest_stories
     else:
         return []
 
-@app.route('/get_time_stories')
-def get_time_stories():
+@app.route('/get_latest_stories')
+def get_latest_stories():
     num_stories = request.args.get('num_stories', default=6, type=int)
-    latest_stories = get_latest_stories(num_stories)
+    latest_stories = fetch_latest_stories(num_stories)
     return jsonify({'latest_stories': latest_stories})
 
 if __name__ == '__main__':
